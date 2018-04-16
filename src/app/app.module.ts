a2fdff74acb532289c0334e453d12c2281315951
AlexanderEklf <37870212+AlexanderEklf@users.noreply.github.com>
2018-04-16 11:05:26 +0200

    Uppdaterar app.module.ts
    Importerar companiesComponent, approutingmodule, httpclientmodule, httpmodule,homecomponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CompaniesComponent } from './companies/companies.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { AppComponent } from './app.component';
 
 // Bootstrap modules
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { HttpModule } from '@angular/http';
+import { HttpClient } from 'selenium-webdriver/http';
+import { HomeComponent } from './home/home.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CompaniesComponent,
+    CompaniesComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     BsDropdownModule.forRoot(),
-    TooltipModule.forRoot()
+    TooltipModule.forRoot(),
+    AppRoutingModule,
+    HttpModule,
+    HttpClientModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
