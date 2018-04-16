550df350fc2572491df667531f2177a8fa85b96a
AlexanderEklf <37870212+AlexanderEklf@users.noreply.github.com>
2018-04-16 11:07:00 +0200

    skapar companies.component
    Tanken är att denna komponent ska hålla samt sköta alla företag som laddats in från databasen.

---
+++ src/app/companies/companies.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Company } from '../models/company';
+import { CompanyService } from '../company.service';
+
+@Component({
+  selector: 'app-companies',
+  templateUrl: './companies.component.html',
+  styleUrls: ['./companies.component.scss'],
+  providers: [CompanyService]
+})
+export class CompaniesComponent implements OnInit {
+
+  companies: Array<Company>;
+
+  constructor(private _companyService: CompanyService) { }
+
+  //TODO:: resten av CRUD operationerna
+  ngOnInit() {
+    this._companyService.getCompanies()
+      .subscribe(resComanyData => this.companies = resComanyData);
+  }
+
+}
