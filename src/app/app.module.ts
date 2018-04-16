f61c5dc598fe02b7af50c1c6f896f95b9854d9b4
Tobias Lans <lanstobias@gmail.com>
2018-04-16 09:11:32 +0200

    flytta navbar till en egen component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { TwbootstrapModule } from './twbootstrap/twbootstrap.module';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
