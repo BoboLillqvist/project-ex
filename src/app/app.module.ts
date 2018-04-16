11818e062ec59e7b63dfbb66f2131d5b436a42cf
Tobias Lans <lanstobias@gmail.com>
2018-04-16 12:23:59 +0200

    ta bort enskilda twbootstrap komponenter från app.module
    Dessa är nu modularesirade och ligger i twbootstrap.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,13 @@ import { NgModule } from '@angular/core';
 import { CompaniesComponent } from './companies/companies.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
-
+import { TwbootstrapModule } from './twbootstrap/twbootstrap.module';
 
 import { AppComponent } from './app.component';
 
-// Bootstrap modules
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
-import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { HttpModule } from '@angular/http';
 import { HttpClient } from 'selenium-webdriver/http';
 import { HomeComponent } from './home/home.component';
-
-import { TwbootstrapModule } from './twbootstrap/twbootstrap.module';
-
-
 import { NavbarComponent } from './layout/navbar/navbar.component';
 
 @NgModule({
@@ -29,8 +22,6 @@ import { NavbarComponent } from './layout/navbar/navbar.component';
   ],
   imports: [
     BrowserModule,
-    BsDropdownModule.forRoot(),
-    TooltipModule.forRoot(),
     AppRoutingModule,
     HttpModule,
     HttpClientModule,
