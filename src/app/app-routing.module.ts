ec6c6cae193f0f287d6ac4df002967513b29f9f3
Alexander N <alexandernordgren@gmail.com>
2018-04-23 16:32:58 +0200

    Lägg till route för /post-exam-work

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,16 @@ import { Routes, RouterModule } from '@angular/router';
 import { CompaniesComponent } from './companies/companies.component';
 import { HomeComponent } from './home/home.component';
 import { AppComponent } from './app.component';
+import { PostExamWorkComponent } from './post-exam-work/post-exam-work.component';
+
 
 
 //Routes sköts här
 const routes: Routes = [
 {path:'', redirectTo:'/home', pathMatch:'full'},
 {path: 'home', component: HomeComponent},
-{path: 'companies', component: CompaniesComponent}
+{path: 'companies', component: CompaniesComponent},
+{path: 'post-exam-work', component: PostExamWorkComponent}
 ];
 
 @NgModule({
