acfd4dd16c56a0c7279bc2a2fbf7d2c4f7500a96
OSandh <oskarsandh@gmail.com>
2018-04-23 18:50:51 +0200

    Fixa ett par instance tester

--- src/app/models/course.model.spec.ts
+++ src/app/models/course.model.spec.ts
@@ -2,6 +2,6 @@ import { Course } from './course.model';
 
 describe('Course', () => {
   it('should create an instance', () => {
-    expect(new Course()).toBeTruthy();
+    expect(new Course('Datateknik')).toBeTruthy();
   });
 });
