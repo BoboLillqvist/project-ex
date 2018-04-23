f9f048a2b70ceda1807722f9c9b311070419cf37
OSandh <oskarsandh@gmail.com>
2018-04-23 10:45:07 +0200

    Skapa schema för student

--- server/models/student.js
+++ server/models/student.js
@@ -1,1 +1,16 @@
+import { Course } from '../../src/app/models/course.model';
+import { Person } from '../../src/app/models/person.model';
 //TODO: schema för studenter som matchar vår modell
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const studentSchema = new Schema({
+    person: Person,
+    name: String,
+    education: String,
+    examYear: Number,
+    skills: String[],
+    courses: Course[]
+});
+
+module.exports = mongoose.model('student', studentSchema, 'students');
