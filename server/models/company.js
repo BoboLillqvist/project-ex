934c5dc8ae008b654df5e136e5e15e32b4997355
AlexanderEklf <37870212+AlexanderEklf@users.noreply.github.com>
2018-04-23 14:26:15 +0200

    Uppdatera schema för company
    ng build &
    node server borde nu fungera korrekt

--- server/models/company.js
+++ server/models/company.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+
 //blueprint av objektet i vår databas
 const companySchema = new Schema({
     _id: String,
     name: String,
     description: String,
     url: String,
-    examWorks: Array<ExamWork>[]
+    examWorks: [{ type: Schema.Types.ObjectId, ref: 'examwork' }]
 });
 
 module.exports = mongoose.model('company', companySchema, 'companies');
