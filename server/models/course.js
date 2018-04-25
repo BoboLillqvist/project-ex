e495f064517d5c7091f0394ea74d3750eb084fdd
OSandh <oskarsandh@gmail.com>
2018-04-25 19:29:25 +0200

    Uppdatera Course schema

--- server/models/course.js
+++ server/models/course.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const courseSchema = new Schema({
+    _id: Schema.Types.ObjectId,
     name: String,
+    points: Number,
 });
 
 module.exports = mongoose.model('course', courseSchema, 'courses');
