5a9fa39d043c8572d59c9ec588eae58e92d7fbc9
Alexander N <alexandernordgren@gmail.com>
2018-04-25 11:04:02 +0200

    Revert "Fix för student.component"
    This reverts commit 35c96d5cc6c3032a808c4bfac429fd3a89ca0705.

--- src/app/students/students.component.ts
+++ src/app/students/students.component.ts
@@ -29,7 +29,7 @@ export class StudentsComponent implements OnInit {
   description: string = '';
   skills: Array<string>;
   courseName: string;
-  courses: Array<Course>;
+  courses: string[] = [];
 
   constructor(private studentService: StudentService) { }
 
