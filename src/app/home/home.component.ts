361d6430d7ee2e2acee1146606895c1c753fe2ae
tnguyen0317 <thong_nguyen7@hotmail.com>
2018-04-26 15:09:44 +0200

    Lägg till testdata i home.component.ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,8 +13,10 @@ export class HomeComponent implements OnInit {
 
   examWork1 = new ExamWork("Program för schemadesign","Örebro",["HTML","CSS","Javascript"],["PHP"],"Erbjudande om examensarbete: Att utveckla och programmera ett program för design av veckoscheman för kurser vid institutionen av Naturvetenskap och teknik. Lämplig gruppstorlek är 1-2 studenter.", new Date("2018-04-23"),"5 dagar i veckan",new Person("Sven","Svensson","Sven_Svensson@yahoo.se","073-4334255"),"nånting",new Company("Apple","Apple Sverige","www.apple.se/about",[this]));
   examWork2 = new ExamWork("MobileRobotic Telepresence Research Platform","Örebro",["C","Van att jobba i linuxmiljö"],["C++"],"Vid intresse, kontakta: [mejl]", new Date("2018-08-20"),"5 dagar i veckan",new Person("Sven","Svensson","Sven_Svensson@yahoo.se","073-4334255"),"nånting",new Company("MRTRP","hurrdurr","www.mobilerobotics.com/about",[this]));
-  examWorks = [this.examWork1,this.examWork2];
-  
+  examWork3 = new ExamWork("MobileRobotic Telepresence2","Örebro",["C","Van att jobba i linuxmiljö"],["C++"],"Ebjudande om examensarbete: hej hejhejhejj Mobile", new Date("2018-08-20"),"5 dagar i veckan",new Person("Sven","Svensson","Sven_Svensson@yahoo.se","073-4334255"),"nånting",new Company("MRTRP","hurrdurr","www.mobilerobotics.com/about",[this]));
+
+  //läs in examensarbeten från databas
+  examWorks = [this.examWork1,this.examWork2,this.examWork3];
 
   constructor() { }
 
