fc91cae50714d4883ec87ebf10bfb12bc3ab792d
Alexander N <alexandernordgren@gmail.com>
2018-04-27 12:57:12 +0200

    Lägg till onSubmitAddCompany() i create-company-profile.component

--- src/app/profiles/create-company-profile/create-company-profile.component.ts
+++ src/app/profiles/create-company-profile/create-company-profile.component.ts
@@ -1,20 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { CompanyService } from '../../company.service';
+import { Company } from '../../models/company.model';
 
 @Component({
   selector: 'app-create-company-profile',
   templateUrl: './create-company-profile.component.html',
-  styleUrls: ['./create-company-profile.component.scss']
   styleUrls: ['./create-company-profile.component.scss'],
   providers: [CompanyService]
 })
 export class CreateCompanyProfileComponent implements OnInit {
 
-  constructor() { }
 
   constructor(private companyService: CompanyService) { }
   
   ngOnInit() {
   }
 
+  onSubmitAddCompany(company: Company){
+    console.log(company.name);
+    console.log(company.url);
+    console.log(company.description);
+    this.companyService.addCompany(company)
+    .subscribe();
+  }
 }
