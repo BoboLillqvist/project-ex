0be65b7a5f50a7d1e0cd267605ac7cdb15319bb8
Alexander N <alexandernordgren@gmail.com>
2018-04-27 12:55:06 +0200

    Lägg till CompanyService i create-company-profile.component

--- src/app/profiles/create-company-profile/create-company-profile.component.ts
+++ src/app/profiles/create-company-profile/create-company-profile.component.ts
@@ -1,14 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { CompanyService } from '../../company.service';
 
 @Component({
   selector: 'app-create-company-profile',
   templateUrl: './create-company-profile.component.html',
   styleUrls: ['./create-company-profile.component.scss']
+  styleUrls: ['./create-company-profile.component.scss'],
+  providers: [CompanyService]
 })
 export class CreateCompanyProfileComponent implements OnInit {
 
   constructor() { }
 
+  constructor(private companyService: CompanyService) { }
+  
   ngOnInit() {
   }
 
