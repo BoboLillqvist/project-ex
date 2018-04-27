cad2a81d04577e24313cee6654a595a402ac4eea
OSandh <oskarsandh@gmail.com>
2018-04-27 17:37:50 +0200

    Lägg till metoden addCourse i student.service

--- src/app/student.service.ts
+++ src/app/student.service.ts
@@ -2,12 +2,14 @@ import { Injectable } from '@angular/core';
 import { Http, Response, Headers, RequestOptions } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { Student } from './models/student.model';
+import { Person } from './models/person.model';
 
 @Injectable()
 export class StudentService {
 
   private _getUrl = '/api/students';
   private _postUrl = '/api/student';
+  private _postUrlPerson = '/api/person';
 
 
   constructor(private _http: Http) { }
@@ -21,6 +23,14 @@ export class StudentService {
     const options = new RequestOptions( {headers: headers } );
     console.log(JSON.stringify(student));
     return this._http.post(this._postUrl, JSON.stringify(student), options).map( (res: Response) => res.json() );
+  }
+
+  addPerson(person: Person) {
+    console.log(person + ' from service');
+    const headers = new Headers( {'Content-Type': 'application/json'} );
+    const options = new RequestOptions( {headers: headers } );
+    console.log(JSON.stringify(person));
+    return this._http.post(this._postUrlPerson, JSON.stringify(person), options).map( (res: Response) => res.json() );
   }
 
 }
