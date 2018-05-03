a95352b962f385f9fae375e4b6f597cff1a421d5
Alexander N <alexandernordgren@gmail.com>
2018-05-03 08:00:26 +0200

    Lägg till addExamWork i examwork.service

--- src/app/examwork.service.ts
+++ src/app/examwork.service.ts
@@ -6,12 +6,20 @@ import { ExamWork } from './models/exam-work.model';
 @Injectable()
 export class ExamworkService {
 
-  private _getUrl = "api/examworks";
+  private _getUrl = "/api/examwork";
+  private _postUrl = "/api/examwork";
 
   constructor(private _http: Http) { }
 
   getCompanies(){
     return this._http.get(this._getUrl)
+      .map((response: Response) => response.json());
+  }
+
+  addExamWork(examwork: ExamWork){
+    let headers = new Headers({ 'Content-Type': 'application/json' });
+    let options = new RequestOptions({ headers: headers});
+    return this._http.post(this._postUrl, JSON.stringify(examwork), options)
       .map((response: Response) => response.json());
   }
 
