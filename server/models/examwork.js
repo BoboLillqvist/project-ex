025fdde8bd2484313e11ffa4ab3803eed0624dcc
Alexander N <alexandernordgren@gmail.com>
2018-05-03 08:01:34 +0200

    Ta bort _id i examworkSchema

--- server/models/examwork.js
+++ server/models/examwork.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const examworkSchema = new Schema({
-    _id: String,
     title: String,
     location: String,
     applyDueDate: Date,
