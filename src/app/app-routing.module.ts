8a327e84113e5f55040d3c7e04dee5c677e64d03
Tobias Lans <lanstobias@gmail.com>
2018-05-03 20:58:14 +0200

    lägg till sidor i router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,18 +7,30 @@ import { PostExamWorkComponent } from './post-exam-work/post-exam-work.component
 import { CreateCompanyProfileComponent } from './profiles/create-company-profile/create-company-profile.component';
 import { StudentsComponent } from './students/students.component';
 import { CreateStudentProfileComponent } from './profiles/create-student-profile/create-student-profile.component';
-
-
-
+import { StudentHomeComponent } from './student-pages/student-home/student-home.component';
+import { StudentProfileComponent } from './student-pages/student-profile/student-profile.component';
+import { EditStudentProfileComponent } from './student-pages/edit-student-profile/edit-student-profile.component';
+import { CompanyHomeComponent } from './company-pages/company-home/company-home.component';
+import { CompanyProfileComponent } from './company-pages/company-profile/company-profile.component';
+import { EditCompanyProfileComponent } from './company-pages/edit-company-profile/edit-company-profile.component';
 
 // Routes sköts här
 const routes: Routes = [
 {path: '', redirectTo: '/home', pathMatch: 'full'},
 {path: 'home', component: HomeComponent},
+
+{path: 'student-home', component: StudentHomeComponent},
+{path: 'student-profile', component: StudentProfileComponent},
+{path: 'edit-student-profile', component: EditStudentProfileComponent},
+{path: 'create-student-profile', component: CreateStudentProfileComponent},
+
+{path: 'company-home', component: CompanyHomeComponent},
+{path: 'company-profile', component: CompanyProfileComponent},
+{path: 'edit-company-profile', component: EditCompanyProfileComponent},
+{path: 'create-company-profile', component: CreateCompanyProfileComponent},
+
 {path: 'companies', component: CompaniesComponent},
 {path: 'post-exam-work', component: PostExamWorkComponent},
-{path: 'create-company-profile', component: CreateCompanyProfileComponent},
-{path: 'create-student-profile', component: CreateStudentProfileComponent},
 {path: 'students', component: StudentsComponent}
 ];
 
