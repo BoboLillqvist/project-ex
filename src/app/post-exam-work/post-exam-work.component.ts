e278f2203734aeddb48ce6b4c7b82b5155eb511f
Alexander N <alexandernordgren@gmail.com>
2018-05-03 07:55:53 +0200

    Lägg till onSubmitAddExamWork i post-exam-work.component

--- src/app/post-exam-work/post-exam-work.component.ts
+++ src/app/post-exam-work/post-exam-work.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ExamworkService } from '../examwork.service';
+import { ExamWork } from '../models/exam-work.model';
+import { NgForm } from '@angular/forms';
+import { Person } from '../models/person.model';
 
 @Component({
   selector: 'app-post-exam-work',
   templateUrl: './post-exam-work.component.html',
-  styleUrls: ['./post-exam-work.component.scss']
+  styleUrls: ['./post-exam-work.component.scss'],
+  providers: [ExamworkService]
 })
 export class PostExamWorkComponent implements OnInit {
 
-  constructor() { }
+  fName: string;
+  lName: string;
+  phoneNbr: string;
+  email: string;
+  constructor(private examWorkService: ExamworkService) { }
 
   ngOnInit() {
   }
 
+  onSubmitAddExamWork(examWork: ExamWork){
+
+
+    console.log("titel: " + examWork.title);
+    console.log("ort: " + examWork.location);
+    console.log("datum: " + examWork.applyDueDate);
+    console.log("ess.skills: " + examWork.essentialSkills);
+    console.log("comp.skills: " + examWork.complementarySkills);
+    console.log("beskrivning: " + examWork.description);
+    console.log("lärdomar: " + examWork.teachings);
+    console.log("company: " + examWork.company);
+    
+    this.examWorkService.addExamWork(examWork)
+    .subscribe();
+
+  }
 }
