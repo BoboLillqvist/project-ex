4d09123766dae1c67873ac1995e9f85c6822e682
Tobias Lans <lanstobias@gmail.com>
2018-05-04 12:06:23 +0200

    lägg till de nya sidorna i routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,10 +9,13 @@ import { StudentsComponent } from './students/students.component';
 import { CreateStudentProfileComponent } from './profiles/create-student-profile/create-student-profile.component';
 import { StudentHomeComponent } from './student-pages/student-home/student-home.component';
 import { StudentProfileComponent } from './student-pages/student-profile/student-profile.component';
+import { StudentViewExamWorkComponent } from './student-pages/student-view-exam-work/student-view-exam-work.component';
 import { EditStudentProfileComponent } from './student-pages/edit-student-profile/edit-student-profile.component';
 import { CompanyHomeComponent } from './company-pages/company-home/company-home.component';
 import { CompanyProfileComponent } from './company-pages/company-profile/company-profile.component';
 import { EditCompanyProfileComponent } from './company-pages/edit-company-profile/edit-company-profile.component';
+import { FindStudentsComponent } from './company-pages/exam-work-pages/find-students/find-students.component';
+import { EditExamWorkComponent } from './company-pages/exam-work-pages/edit-exam-work/edit-exam-work.component';
 
 // Routes sköts här
 const routes: Routes = [
@@ -25,9 +28,13 @@ const routes: Routes = [
 {path: 'student/home', component: StudentHomeComponent},
 {path: 'student/profile', component: StudentProfileComponent},
 {path: 'student/edit-profile', component: EditStudentProfileComponent},
+{path: 'student/view-exam-work', component: StudentViewExamWorkComponent},
+
 {path: 'company/home', component: CompanyHomeComponent},
 {path: 'company/profile', component: CompanyProfileComponent},
 {path: 'company/edit-profile', component: EditCompanyProfileComponent},
+{path: 'company/exam-work/find-students', component: FindStudentsComponent},
+{path: 'company/exam-work/edit-exam-work', component: EditExamWorkComponent},
 {path: 'company/post-exam-work', component: PostExamWorkComponent},
 
 {path: 'companies', component: CompaniesComponent},
