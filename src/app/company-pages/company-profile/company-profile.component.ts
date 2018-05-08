b1bb3c7edc040572d3a42c68d0c1366f0df22956
OSandh <oskarsandh@gmail.com>
2018-05-08 17:44:49 +0200

    Fyll ut company-profile komponenten.

--- src/app/company-pages/company-profile/company-profile.component.ts
+++ src/app/company-pages/company-profile/company-profile.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { Company } from '../../models/company.model';
+import { CompanyService } from '../../company.service';
+import { ExamWork } from '../../models/exam-work.model';
+import { Person } from '../../models/person.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-company-profile',
   templateUrl: './company-profile.component.html',
-  styleUrls: ['./company-profile.component.scss']
+  styleUrls: ['./company-profile.component.scss'],
+  providers: [CompanyService]
 })
 export class CompanyProfileComponent implements OnInit {
 
-  constructor() { }
+  company: Company;
+  contact: Person;
+
+  constructor(private companyService: CompanyService, private router: Router) { }
 
   ngOnInit() {
+    // för ng serve develop
+    this.contact = new Person('Monti', 'Berg', 'montiberg321@ggcorp.co', '070555111');
+    const ew = [new ExamWork('Work1', 'Örebro', ['c#', 'js'], ['.net'], 'do this and that', Date.now, '100%', '', '', ''),
+                new ExamWork('Work2', 'Örebro', ['c#', 'js'], ['.net'], 'do this and that', Date.now, '100%', '', '', ''),
+                new ExamWork('Work3', 'Örebro', ['c#', 'js'], ['.net'], 'do this and that', Date.now, '100%', '', '', ''),
+                new ExamWork('Work4', 'Örebro', ['c#', 'js'], ['.net'], 'do this and that', Date.now, '100%', '', '', ''),
+                new ExamWork('Work5', 'Örebro', ['c#', 'js'], ['.net'], 'do this and that', Date.now, '100%', '', '', ''),
+                new ExamWork('Work6', 'Örebro', ['c#', 'js'], ['.net'], 'do this and that', Date.now, '100%', '', '', '')];
+    // this.company = new Company('GG Corp', 'ggnore. dslöfkaöls afaödsf asf öasf alsö asdf a aio aweihfiu asiudhf alks', 'http://gg.co', ew);
+
+    this.company = new Company('', '', '', []);
+    const _id = this.companyService._id;
+    this.companyService.getCompany(_id).subscribe( resCompData => {
+      this.company = resCompData;
+      this.company.examWorks = ew;
+    });
+  }
+
   goToUrl() {
     let url: string = '';
     if (!/^http[s]?:\/\//.test(this.company.url)) {
