14c3f73d9bb1db92d138f78b179295c3c4ed58ba
Alexander N <alexandernordgren@gmail.com>
2018-05-08 09:04:23 +0200

    Lägg till RegisterChoice i app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -30,6 +30,7 @@ import { FindStudentsComponent } from './company-pages/exam-work-pages/find-stud
 import { EditExamWorkComponent } from './company-pages/exam-work-pages/edit-exam-work/edit-exam-work.component';
 import { LoginComponent } from './login/login.component';
 import { CancleComponent } from './cancle/cancle.component';
+import { RegisterChoiceComponent } from './register-choice/register-choice.component';
 
 @NgModule({
   declarations: [
@@ -58,6 +59,7 @@ import { CancleComponent } from './cancle/cancle.component';
     EditExamWorkComponent,
     LoginComponent,
     CancleComponent,
+    RegisterChoiceComponent,
   ],
   imports: [
     BrowserModule,
