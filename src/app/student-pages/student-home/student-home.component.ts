c7a3609cc49584cb65cf063533a830ef1778dae0
OSandh <oskarsandh@gmail.com>
2018-05-09 16:35:20 +0200

    Lägg till så examworks hämtas från databasen i student-home.component

--- src/app/student-pages/student-home/student-home.component.ts
+++ src/app/student-pages/student-home/student-home.component.ts
@@ -1,17 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { ExamWork } from '../../models/exam-work.model';
+import { ExamworkService } from '../../examwork.service';
 
 @Component({
   selector: 'app-student-home',
   templateUrl: './student-home.component.html',
-  styleUrls: ['./student-home.component.scss']
+  styleUrls: ['./student-home.component.scss'],
+  providers: [ExamworkService]
 })
 export class StudentHomeComponent implements OnInit {
 
-  constructor() { }
   examWorks: Array<ExamWork> = [];
 
+  constructor(private examService: ExamworkService) { }
+
   ngOnInit() {
+    this.examService.getExamWorks().subscribe( resData => {
+      this.examWorks = resData;
+    });
   }
 
 }
