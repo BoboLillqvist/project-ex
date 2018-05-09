8efa72c596e73609d4267546e3f8c7309ab84b78
OSandh <oskarsandh@gmail.com>
2018-05-09 17:17:55 +0200

    Ändra hur företag läses in i company-profile

--- src/app/company-pages/company-profile/company-profile.component.ts
+++ src/app/company-pages/company-profile/company-profile.component.ts
@@ -21,20 +21,13 @@ export class CompanyProfileComponent implements OnInit {
   ngOnInit() {
     // för ng serve develop
     this.contact = new Person('Monti', 'Berg', 'montiberg321@ggcorp.co', '070555111');
-    const ew = [new ExamWork('Work1', 'Örebro', ['c#', 'js'], ['.net'], 'do this and that', Date.now, '100%', '', '', ''),
-                new ExamWork('Work2', 'Örebro', ['c#', 'js'], ['.net'], 'do this and that', Date.now, '100%', '', '', ''),
-                new ExamWork('Work3', 'Örebro', ['c#', 'js'], ['.net'], 'do this and that', Date.now, '100%', '', '', ''),
-                new ExamWork('Work4', 'Örebro', ['c#', 'js'], ['.net'], 'do this and that', Date.now, '100%', '', '', ''),
-                new ExamWork('Work5', 'Örebro', ['c#', 'js'], ['.net'], 'do this and that', Date.now, '100%', '', '', ''),
-                new ExamWork('Work6', 'Örebro', ['c#', 'js'], ['.net'], 'do this and that', Date.now, '100%', '', '', '')];
-    // this.company = new Company('GG Corp', 'ggnore. dslöfkaöls afaödsf asf öasf alsö asdf a aio aweihfiu asiudhf alks', 'http://gg.co', ew);
+
 
     this.company = new Company('', '', '', []);
+
     const _id = this.companyService._id;
-    this.companyService.getCompany(_id).subscribe( resCompData => {
-      this.company = resCompData;
-      this.company.examWorks = ew;
-    });
+
+    this.companyService.getCompany(_id).subscribe( resCompData => this.company = resCompData);
   }
 
   goToUrl() {
