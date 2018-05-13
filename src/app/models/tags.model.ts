0bd4635e92717b04fb3d2f1c1324eab29e90e0f4
Tobias Lans <lanstobias@gmail.com>
2018-05-13 21:13:24 +0200

    skapa Tags modell

---
+++ src/app/models/tags.model.ts
@@ -0,0 +1,10 @@
+export class Tags {
+    _id: string;
+    type: string;
+    values: Array<String>;
+
+    constructor(type, values) {
+        this.type = name;
+        this.values = values;
+    }
+}
