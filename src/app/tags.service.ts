ee318b2709338e70ddd3e9c965c0bfe23c98be84
Tobias Lans <lanstobias@gmail.com>
2018-05-13 21:13:56 +0200

    skapa tags.service

---
+++ src/app/tags.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+
+@Injectable()
+export class TagsService {
+  private getUrl = 'api/tags';
+
+  constructor(private http: Http) { }
+
+  getTags() {
+    return this.http.get(this.getUrl)
+      .map((response: Response) => response.json());
+  }
+}
