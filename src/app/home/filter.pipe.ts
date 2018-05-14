ba26794716163c482275bfdd748e48fc30775b5d
tnguyen0317 <thong_nguyen7@hotmail.com>
2018-05-14 12:16:53 +0200

    Städa upp lite i filter.pipe

--- src/app/home/filter.pipe.ts
+++ src/app/home/filter.pipe.ts
@@ -1,36 +1,11 @@
 import { Pipe, PipeTransform } from '@angular/core';
-
+import { Student } from '../models/student.model';
 
 @Pipe({
-    name: 'filter'
+    name: 'filter',
+    pure: false,
 })
 export class FilterPipe implements PipeTransform {
-  transform(items: any, filter: any, filterItems: Array<any>, isAnd: boolean): any {
-    console.log('Filtering ..');
-    if (filter && Array.isArray(items) && filterItems) {
-      let filterKeys = Object.keys(filter);
-      let checkedItems = filterItems.filter(item => { return item.checked; });
-      if (!checkedItems || checkedItems.length === 0) { return items; }
-      if (isAnd) {
-        return items.filter(item =>
-            filterKeys.reduce((acc1, keyName) =>
-                (acc1 && checkedItems.reduce((acc2, checkedItem) => acc2 && new RegExp(item[keyName], 'gi').test(checkedItem.value) || checkedItem.value === "", true))
-              , true)
-              );
-      } else {
-        return items.filter(item => {
-          return filterKeys.some((keyName) => {
-            return checkedItems.some((checkedItem) => {
-              return new RegExp(item[keyName], 'gi').test(checkedItem.value) || checkedItem.value === "";
-            });
-          });
-        });
-      }
-    } else {
-      return items;
-    }
-  }
-}
     transform(items: any[], filter: Array<String>): any {
         if (filter === undefined)
             return items;
@@ -38,14 +13,6 @@ export class FilterPipe implements PipeTransform {
         let sortedArray = [];
 
 
-// @Pipe({
-//     name: 'filter',
-//     pure: false,
-// })
-// export class FilterPipe implements PipeTransform {
-//     transform(items: any, filter: string): any {
-//         if (filter === undefined)
-//             return items;
         items.forEach(item => {
             filter.forEach(skill => {
                 item.skills.forEach(essSkill => {
@@ -54,25 +21,10 @@ export class FilterPipe implements PipeTransform {
                 });
             })
 
-//         //items som matchar och returnerar true är kvar, de som är false filtreras ut
 
-//             return items.filter(item => item.name.toLowerCase().indexOf(filter.toLowerCase()) !== -1);
-//         }
-//     }
         })
 
         return sortedArray;
 
-// @Pipe({
-//     name: 'filter2',
-//     pure: false,
-// })
-// export class FilterDescriptionPipe implements PipeTransform {
-//     transform(items: any[], filter: string): any {
-//         if (!items || !filter) {
-//             return items;
-//         }
-
-//         return items.filter(item => item.gender.toLowerCase().indexOf(filter.toLowerCase()) !== -1);
-//     }
-// }
+        }
+    }
