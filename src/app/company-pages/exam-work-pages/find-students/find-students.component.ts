4b914fc35fe7ea2e6805fe3e02e090f0d36f6c6d
AlexanderEklof <37870212+AlexanderEklf@users.noreply.github.com>
2018-05-14 11:57:53 +0200

    Uppdaterar find-students-komponenten
    Komponenten hämtar nu studenter från databasen

--- src/app/company-pages/exam-work-pages/find-students/find-students.component.ts
+++ src/app/company-pages/exam-work-pages/find-students/find-students.component.ts
@@ -1,33 +1,54 @@
 import { Component, OnInit } from '@angular/core';
-import { Student } from '../../../models/student.model'
+import { Student } from '../../../models/student.model';
 import { Course } from '../../../models/course.model';
+import { StudentService } from '../../../student.service';
+import { ActivatedRoute, Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-find-students',
   templateUrl: './find-students.component.html',
-  styleUrls: ['./find-students.component.scss']
+  styleUrls: ['./find-students.component.scss'],
+  providers: [StudentService]
 })
 export class FindStudentsComponent implements OnInit {
 
+  Students: Array<Student>;
+  Student: Student;
+  StudentId: String;
 
-  constructor() {
 
-    //TODO: hämta alla students från databasen
+
+  constructor(private studentService: StudentService, private route: ActivatedRoute) {
     // temporära hårdkodade students
-    let Students = [
-      new Student('Sven','Svensson','Maskiningenjör',2019,'Gillar att äta glass',
-      ['Matematik','Fysik'],[new Course('Matematik I',7.5), new Course('Maskinteknik A',7.5)],
-      'Sven_Svensson1337@hotmail.com','07013371337'),
-      new Student('Zven','Svensson','Dataingenjör',2021,
-      'Nånting nånting nånting hej hej hej tja',['C','Go','Rust','FORTRAN'],[new Course('Matematik I',7.5),new Course('Objektorienterad programmering',7.5)],
-      'cool_grabb1717@yahoo.se','0707070707')
-  ]
+
+
+  //   let Students = [
+  //     new Student('Sven','Svensson','Maskiningenjör',2019,'Gillar att äta glass',
+  //     ['Matematik','Fysik'],[new Course('Matematik I',7.5), new Course('Maskinteknik A',7.5)],
+  //     'Sven_Svensson1337@hotmail.com','07013371337'),
+  //     new Student('Zven','Svensson','Dataingenjör',2021,
+  //     'Nånting nånting nånting hej hej hej tja',['C','Go','Rust','FORTRAN'],
+  //     [new Course('Matematik I',7.5),new Course('Objektorienterad programmering',7.5)],
+  //     'cool_grabb1717@yahoo.se','0707070707')
+  // ]
    }
 
-  ngOnInit() {
-    
+  onClickViewStudent() {
+    this.StudentId = this.route.snapshot.params['id'];
+
   }
 
-  
+
+  ngOnInit() {
+    this.studentService.getStudents().subscribe(resStudentData => {
+      this.Students = resStudentData;
+    });
+
+  }
+
+
+
+
 
 }
