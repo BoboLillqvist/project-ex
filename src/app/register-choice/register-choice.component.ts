14aa076a91e9aa4179d0154ba657381654dcab3b
Alexander N <alexandernordgren@gmail.com>
2018-05-14 09:51:00 +0200

    Ändra länk till bakgrund för register-choice

--- src/app/register-choice/register-choice.component.ts
+++ src/app/register-choice/register-choice.component.ts
@@ -11,7 +11,7 @@ export class RegisterChoiceComponent implements OnInit {
 
   ngOnInit() {
     document.getElementById("main-navbar").setAttribute('hidden', 'true');
-    document.body.style.backgroundImage = "url('https://image.ibb.co/ediGeS/wp_blue.jpg')"; 
+    document.body.style.backgroundImage = "url('https://firebasestorage.googleapis.com/v0/b/firstcontact-3ad7f.appspot.com/o/background%2Fwp_blue.jpg?alt=media&token=d30c846b-0907-45b2-a7bc-19eb1a549212')"; 
   }
 
 }
