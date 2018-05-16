65cad7a3e858d6cb8bbf4d4e9caa57f19a2bbab1
OSandh <oskarsandh@gmail.com>
2018-05-16 14:55:51 +0200

    Hämta hem rätt företag från databas i edit-company-profile

--- src/app/components/company-pages/edit-company-profile/edit-company-profile.component.ts
+++ src/app/components/company-pages/edit-company-profile/edit-company-profile.component.ts
@@ -1,23 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 import { Company } from '../../../models/company.model';
+import { CompanyService } from '../../../services/company.service';
 import { Person } from '../../../models/person.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-edit-company-profile',
   templateUrl: './edit-company-profile.component.html',
-  styleUrls: ['./edit-company-profile.component.scss']
+  styleUrls: ['./edit-company-profile.component.scss'],
+  providers: [CompanyService]
 })
 export class EditCompanyProfileComponent implements OnInit {
 
-  constructor() { }
   company: Company;
   contact: Person;
 
   backupCompany: Company;
   backupContact: Person;
 
+  constructor(private compServ: CompanyService, private router: Router) {
+    this.company = new Company('ÖRebro uni', 'Bra uni du vet', 'https://oru.se', []);
+    this.contact = new Person('', '', '', '');
+  }
 
   ngOnInit() {
+    // hårdkodat id hämtas nu
+    const _id = this.compServ._id;
+
+    this.compServ.getCompany(_id).subscribe(resData => {
+      this.company = resData;
+
+      // deep copy
+      this.backupCompany = Object.assign({}, this.company);
+      this.backupContact = Object.assign({}, this.contact);
+    });
+  }
   }
 
 }
