3bbfd8b5af20dcce6247d235c2c1b08135b72fe3
OSandh <oskarsandh@gmail.com>
2018-05-20 19:27:43 +0200

    Importera service samt skapa medlemsvariabler
    Och uppdatera html

--- src/app/components/register-login/login/login.component.ts
+++ src/app/components/register-login/login/login.component.ts
@@ -1,13 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { UserAuthService } from '../../../services/user-auth.service';
+import { Router } from '@angular/router';
+import { User } from '../../../models/user.model';
+import { Student } from '../../../models/student.model';
+import { Company } from '../../../models/company.model';
+import { StudentService } from '../../../services/student.service';
+import { CompanyService } from '../../../services/company.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
+  providers: [UserAuthService, StudentService, CompanyService]
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  user: User;
+  student: Student;
+  company: Company;
+  validInfo: boolean = true;
+
+  constructor(private auth: UserAuthService, private router: Router,
+              private studServ: StudentService, private compServ: CompanyService) {
+    this.user = new User('', '', '', '');
+  }
 
   ngOnInit() {
     document.getElementById("main-navbar").setAttribute('hidden', 'true');
