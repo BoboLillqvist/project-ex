0026821a1a8cd3c63ddde227f615773d54096e4b
BoboLillqvist <bobo.lillqvist@gmail.com>
2018-05-21 13:01:34 +0200

    Lägg till grunden så att examensProfilsidan i company hänvisar till student viewen

--- src/app/components/exam-work-pages/company/exam-work-dashboard/exam-work-dashboard.component.ts
+++ src/app/components/exam-work-pages/company/exam-work-dashboard/exam-work-dashboard.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild} from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ExamWork } from '../../../../models/exam-work.model';
 import { Person } from '../../../../models/person.model';
 import { Company } from '../../../../models/company.model';
@@ -8,6 +8,7 @@ import { ProgressBarComponent } from '../../../misc/progress-bar/progress-bar.co
 import * as moment from 'moment';
 import { StudentService } from '../../../../services/student.service';
 import { Student } from '../../../../models/student.model';
+import { User } from '../../../../models/user.model';
 
 @Component({
   selector: 'app-exam-work-dashboard',
@@ -21,17 +22,19 @@ export class ExamWorkDashboardComponent implements OnInit {
   examWorkId: String;
   examWork: ExamWork;
   students: Array<Student> = [];
+  user: User;
   constructor(
     private examService: ExamworkService,
     private studentService: StudentService,
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private router: Router
   ) {
     this.examWorkId = this.activatedRoute.snapshot.params['id'];
     this.examWork = new ExamWork('', '', '', [], [], '', '',
                                  new Person('', '', '', ''), '',
                                  new Company('', '', '', [])
     );
-
+    this.user = new User('', '', '', '');
   }
 
   ngOnInit() {
@@ -39,6 +42,22 @@ export class ExamWorkDashboardComponent implements OnInit {
       .subscribe(fetchedExamWork => this.examWork = fetchedExamWork);
     this.studentService.getStudents()
       .subscribe(fetchedStudents => this.students = fetchedStudents);
+
+       //TODO: byt ut denna mot vem som är inloggad.
+       if (document.URL.includes('company')) {
+        this.user.role = 'company';
+      } else {
+        this.user.role = 'student';
+      }
+    this.goToPathBasedOnUserRole(this.user._id, this.examWorkId)
     console.log(this.examWork);
+  }
+
+  goToPathBasedOnUserRole(roleId, exWorkId) {
+    let path;
+    if (this.user.role === 'student') {
+      path = '/student/view-exam-work/' + exWorkId;
+      this.router.navigateByUrl(path);
+    }
   }
 }
