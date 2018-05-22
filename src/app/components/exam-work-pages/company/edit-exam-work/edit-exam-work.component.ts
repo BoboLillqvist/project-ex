a88f5d214d674565b33506dc92d159be72988bed
Tobias Lans <lanstobias@gmail.com>
2018-05-22 13:53:11 +0200

    lägg till dependencies för EditExamWorkComponent

--- src/app/components/exam-work-pages/company/edit-exam-work/edit-exam-work.component.ts
+++ src/app/components/exam-work-pages/company/edit-exam-work/edit-exam-work.component.ts
@@ -1,9 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ExamworkService } from '../../../../services/examwork.service';
+import { ExamWork } from '../../../../models/exam-work.model';
+import { Person } from '../../../../models/person.model';
+import { Company } from '../../../../models/company.model';
+import { Router } from '@angular/router';
+import { EditStudentSkillsComponent } from './edit-student-skills/edit-student-skills.component';
+import { TagsService } from '../../../../services/tags.service';
+import { PersonService } from '../../../../services/person.service';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-edit-exam-work',
   templateUrl: './edit-exam-work.component.html',
-  styleUrls: ['./edit-exam-work.component.scss']
+  styleUrls: ['./edit-exam-work.component.scss'],
+  providers: [
+    ExamworkService,
+    TagsService,
+    PersonService
+  ]
 })
 export class EditExamWorkComponent implements OnInit {
 
