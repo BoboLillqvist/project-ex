5665fa807f6c842d290c6f64609a5df85a4bf6ec
OSandh <oskarsandh@gmail.com>
2018-05-23 11:15:54 +0200

    Lägg till logout() i navbar.
    Och bind till knapparna

--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserAuthService } from '../../services/user-auth.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -9,8 +11,15 @@ export class NavbarComponent implements OnInit {
 
   isCollapsed: boolean = true;
 
-  constructor() { }
+  constructor(private auth: UserAuthService,
+              private router: Router
+            ) { }
 
   ngOnInit() {
+  }
+
+  logout() {
+    this.auth.logout();
+    this.router.navigateByUrl('/login');
   }
 }
