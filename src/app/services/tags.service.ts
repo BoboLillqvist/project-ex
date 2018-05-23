c88c5d812b7ccb32472e0bc1864d40fd764647c8
OSandh <oskarsandh@gmail.com>
2018-05-23 10:43:39 +0200

    tags.service Http > HttpClient

--- src/app/services/tags.service.ts
+++ src/app/services/tags.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
 import { Tags } from '../models/tags.model';
 
 @Injectable()
@@ -7,24 +7,23 @@ export class TagsService {
   private getUrl = 'api/tags';
   private putUrl = 'api/tags';
 
-  constructor(private http: Http) { }
+  constructor(private http: HttpClient) { }
 
   getAvailableTags() {
     return this.http.get(this.getUrl)
-      .map((response: Response) => response.json());
+      .map((response: Response) => response);
   }
 
   updateAvailableTags(tags: Tags, storedTags: any) {
-    const headers = new Headers({ 'Content-Type': 'application/json' });
-    const options = new RequestOptions({ headers: headers});
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
 
     // Store new union set in tags value
     const addedValuesUnion = this.union(storedTags.essentials, storedTags.complimentary);
     const allTagsUnion = this.union(addedValuesUnion, tags.values);
     tags.values = allTagsUnion;
 
-    return this.http.put(this.putUrl + tags._id, JSON.stringify(tags), options)
-      .map((response: Response) => response.json());
+    return this.http.put(this.putUrl + tags._id, JSON.stringify(tags), {headers})
+      .map((response: Response) => response);
   }
 
   // Source: https://stackoverflow.com/a/23080662
