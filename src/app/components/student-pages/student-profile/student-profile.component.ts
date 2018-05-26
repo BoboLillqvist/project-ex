185ea6195be079ffcf81e51d7dc21f809388b22a
OSandh <oskarsandh@gmail.com>
2018-05-26 10:33:24 +0200

    Fixa så student profile laddas in

--- src/app/components/student-pages/student-profile/student-profile.component.ts
+++ src/app/components/student-pages/student-profile/student-profile.component.ts
@@ -32,7 +32,8 @@ export class StudentProfileComponent implements OnInit {
     this.student = new Student('', '', '', 0, '', [], [], '', '');
     // TODO: hämta in student/student id från någonstans. Kanske kan hamna i någon Auth service vid login?
     let studId: string = this.route.snapshot.params['id'];  // tar in hårdkodat id just nu
-    if (studId === 'profile') {
+    
+    if (studId === undefined) {
       studId = this.auth.getRoleId();
       this.myProfile = true;
     }
